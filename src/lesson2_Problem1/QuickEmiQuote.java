b3a3d14da253dd93c52e85eb519f13b4b96af211
LiyaT16 <liyahabtu03@gmail.com>
2025-09-01 21:03:33 -0500

    First commit

--- src/lesson2_Problem1/QuickEmiQuote.java
+++ src/lesson2_Problem1/QuickEmiQuote.java
@@ -1,7 +1,53 @@
 package lesson2_Problem1;
 
+import java.util.Random;
+
 public class QuickEmiQuote {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //#1
+        Random r = new Random();
+        int p = r.nextInt(5000,25000);
+        int n = r.nextInt(6,36);
+        double APR = r.nextDouble(3.5,11.5);
+
+        //#2
+        double rate = APR/ 12 / 100 ;
+        double EMI = (p * rate * Math.pow(1 + rate , n))/ (Math.pow(1 + rate , n) - 1);
+        double totalPayment = EMI * n;
+        double totalInterest = totalPayment - p;
+
+        //#3
+        int cast = (int)  EMI;
+        long round = Math.round(EMI);
+
+        //#4
+        System.out.println("========================================================");
+        System.out.println("Quick Emi Quote Calculator");
+        System.out.println("========================================================");
+        System.out.printf("Principal = $%,d%n", p);
+        System.out.printf("Tenure = %d months%n", n);
+        System.out.printf("APR = %.1f%%\n", APR);
+        System.out.println("________________________________________________________");
+        System.out.printf("EMI = $%.2f%n ", EMI);
+        System.out.printf("Total Payment = $%.2f%n" , totalPayment );
+        System.out.printf("Total Interest = $%.2f%n" , totalInterest );
+        System.out.println("________________________________________________________");
+        System.out.printf("Cast = $%d%n" , cast);
+        System.out.printf("Round = $%d%n" , round);
+        System.out.println("========================================================");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
