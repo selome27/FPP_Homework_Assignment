cab9006f3fd0db33f99f2645e3a5efabf25e8b15
selome27 <selometas21@gmail.com>
2025-09-18 19:55:58 -0500

    problem3

--- src/LESSON9/problem3/BrowserHistory.java
+++ src/LESSON9/problem3/BrowserHistory.java
@@ -1,4 +1,56 @@
 package LESSON9.problem3;
 
-public class BrowserHistory {
+    import java.util.Stack;
+
+    public class BrowserHistory {
+
+
+        private Stack<String> back;
+        private Stack<String> forward;
+        private String currentUrl;
+
+
+        public BrowserHistory(String homepage) {
+            back = new Stack<>();
+            forward = new Stack<>();
+            currentUrl = homepage;
+        }
+    public void visit(String url)
+    // for visiting a new url
+    //it should update the current url
+    //manage the history stacks
+    {
+        back.push(currentUrl);
+        currentUrl = url;
+        forward.clear();
+        System.out.println("Visited" + currentUrl);
+    }
+    public void back()
+    //should allow the user to navigate back to
+    //the previous URL in the history
+    {
+        if (!back.isEmpty()) {
+            forward.push(currentUrl);
+            currentUrl = back.pop();
+            System.out.println("Back to: " + currentUrl);
+        } else {
+            System.out.println("No history to go back to.");
+        }
+
+    }
+    public void forward()
+    //should allow the user to navigate forward
+    {
+         if(!forward.isEmpty()){
+             back.push(currentUrl);
+             currentUrl = forward.pop();
+             System.out.println("Forward" + currentUrl);
+         }
+         else {
+             System.out.println("No Forward history");
+         }
+
+    }
+
+
 }
--- src/LESSON9/problem3/TestBrowserHistory.java
+++ src/LESSON9/problem3/TestBrowserHistory.java
@@ -1,5 +1,18 @@
 package LESSON9.problem3;
 
 public class TestBrowserHistory {
-    
+    public static void main(String[] args) {
+        BrowserHistory browser = new
+                BrowserHistory("home.html");// Start with home.html
+        browser.visit("page1.html");
+        browser.visit("page2.html");
+        browser.back();
+        browser.back();
+        browser.back();
+
+        browser.forward();
+        browser.forward();
+        browser.forward();
+        browser.visit("page3.html");
+    }
 }
