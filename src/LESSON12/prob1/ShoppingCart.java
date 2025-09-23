01cc69eb3f7901653c80ee87fe7643d6b7b4e41e
selome27 <selometas21@gmail.com>
2025-09-22 19:39:31 -0500

    second commit

--- src/LESSON12/prob1/ShoppingCart.java
+++ src/LESSON12/prob1/ShoppingCart.java
@@ -9,22 +9,24 @@ public class ShoppingCart {
         Scanner sc = new Scanner(System.in);
 
         boolean valid = false;
-        try{
-        System.out.print("Enter quantity of items to add (1-50): ");
+        while (!valid) {
+            try {
+                System.out.print("Enter quantity of items to add (1-50): ");
 
-        int quantity = sc.nextInt();
-            if (quantity < 1 || quantity > 50) {
-                throw new IllegalArgumentException("Quantity must be between 1 and 50.");
+                int quantity = sc.nextInt();
+                if (quantity < 1 || quantity > 50) {
+                    throw new IllegalArgumentException("Quantity must be between 1 and 50.");
+                }
+                valid = true;
+                System.out.println("Successfully added " + quantity + " items to your cart!");
+
+            } catch (InputMismatchException e) {
+                System.out.println("The input is not working");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
-            valid =true;
-        System.out.println("Successfully added " + quantity + " items to your cart!");
+            sc.close();
+        }
 
-    }catch (InputMismatchException e){
-            System.out.println("The input is not working");
-        } catch (IllegalArgumentException e) {
-        System.out.println(e.getMessage());
     }
-        sc.close();
-    }
-
 }
